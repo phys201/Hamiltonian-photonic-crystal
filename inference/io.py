7941ac3b7ffa580c7f3aa55d368fd8bbbcfc3b5a
pablotting <yiting_huang@college.harvard.edu>
2023-04-19 14:30:59 -0400

    Unit Test Update
    We update our unit test cases. It cannot run as of now due to incompatibility between io.py and model.py

--- inference/io.py
+++ inference/io.py
@@ -1,14 +1,15 @@
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
 import xarray as xr
 import pandas as pd
-import pymc as pm
 import scipy.signal as sg
 from matplotlib import rcParams
-import cv2 as cv
 from scipy import interpolate 
+
+    
 def io(filename):
     da = xr.open_dataset(filename)
+    print(da)
     return da
+    # def __init__(self,filename):
+    #     self.da = xr.open_dataset(filename)
+    #     return self.da
