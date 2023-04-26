58eb4abc51e8240eaf0315940c83e82fbeb0ab42
thaodinh98 <tdinh@g.harvard.edu>
2023-04-26 17:26:04 -0400

    fix documentation
    I fix the documentation, and io.py

--- photonic_hamiltonian/io.py
+++ photonic_hamiltonian/io.py
@@ -1,15 +1,21 @@
 import numpy as np
 import xarray as xr
-import pandas as pd
-import scipy.signal as sg
-from matplotlib import rcParams
-from scipy import interpolate 
 
+def load_data(filename):
+    """
+    returns a xarray of the data in a .npy format data file
     
-def io(filename):
-    da = xr.open_dataset(filename)
-    print(da)
-    return da
-    # def __init__(self,filename):
-    #     self.da = xr.open_dataset(filename)
-    #     return self.da
+    Parameters:
+        filename (Str): filename of the .npy data file, make sure to include the correct file path    
+        
+    Return:
+        data (Xarray): data with labels 'normf', 'spectrum', 'spectrum_std'
+    """
+    np_data = np.load(filename)
+    x = np_data[0]
+    y = np_data[1]
+    dy = np_data[2]
+    data = xr.Dataset(data_vars = {'spectrum': ('normf', y),
+                                  'spectrum_std': ('normf', dy)},
+                     coords = { 'normf': x})
+    return data
