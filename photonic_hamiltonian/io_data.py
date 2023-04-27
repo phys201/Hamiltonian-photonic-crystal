64ef8ee4df0112e129a6a46de63072d2b6b2bb84
thaodinh98 <tdinh@g.harvard.edu>
2023-04-26 22:06:23 -0400

    Update documentation

--- photonic_hamiltonian/io_data.py
+++ photonic_hamiltonian/io_data.py
@@ -1,8 +1,6 @@
-# 'io_data.py' works as required 'io.py' (there is something with the same name of 'io' on Fan's PC that causes error, so this file has to use another name)
 # io_data.py is used to read data into generative model
 # filename is the file that stored the simulated/experimental data
-# all the processed data are in the same form, saved in dataarray
-
+# all the processed data are in the same form, saved in DataArray
 
 import numpy as np
 import xarray as xr
@@ -14,5 +12,5 @@ def io_data(filename):
     meta data (simu): information of simulation paramenters
     """
     da = xr.open_dataset(filename)
-    print('information of loaded data: '+ da.attrs)
+    #print('information of loaded data: '+ da.attrs)
     return da
